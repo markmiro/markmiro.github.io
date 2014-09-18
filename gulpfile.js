4e6c99f93cae7d1af276faa4412b2a44a3c3b07f
Mark Miro <markmirony@gmail.com>
2014-09-18 13:52:25 -0700

    Deploying to remote branch

--- gulpfile.js
+++ gulpfile.js
@@ -23,3 +23,13 @@ gulp.task('browser-sync', function () {
             }
     });
 });
+
+
+var deploy = require("gulp-gh-pages");
+var options = { 
+    remoteUrl: "https://github.com/markmiro/markmiro.github.io.git",
+    branch: "dist"};
+gulp.task('deploy', function () {
+    gulp.src("dist/**/*")
+        .pipe(deploy(options));
+});
