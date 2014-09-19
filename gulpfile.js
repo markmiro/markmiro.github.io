e8db700da6e9975ddc8825b8240efd733b183a48
Mark Miro <markmirony@gmail.com>
2014-09-19 15:14:54 -0700

    Basic setup done for building js, css, html, and watching

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,78 @@
 var gulp = require('gulp'),
+    runSequence = require('run-sequence'),
     browserSync = require('browser-sync'),
-    clean = require('gulp-clean'),
+    clean = require('gulp-rimraf'),
     sass = require('gulp-sass'),
+    colorguard = require('gulp-colorguard'),
     jade = require('gulp-jade');
 
-gulp.task('default', function() {
+
+
+
+var SRC = {
+    JADE: 'src/**/*.jade',
+    JS: 'src/**/*.js',
+    SASS: 'src/scss/**/*.scss'
+};
+
+gulp.task('build-jade', function () {
     var LOCALS = {};
 
-    gulp.src('dist', {read: false})
-        .pipe(clean());
-
-    gulp.src('src/**/*.jade')
+    return gulp.src(SRC.JADE)
         .pipe(jade({ locals: LOCALS }))
         .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({ stream:true }));
+});
 
-    gulp.src('src/**/*.js')
-        .pipe(gulp.dest('dist'))
+gulp.task('build-sass', function () {
+    return gulp.src(SRC.SASS)
+        .pipe(sass({ sourceComments: 'map' }))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({ stream:true }));
+});
 
-    gulp.src('src/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/styles'))
-
-    gulp.src('src/CNAME')
-        .pipe(gulp.dest('dist'))
-
-    // gulp.src('src/**/*')
-    //     .pipe(gulp.dest('dist'));
+gulp.task('build-js', function () {
+    return gulp.src(SRC.JS)
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('browser-sync', function () {
     var files = [
             '*.html',
-            'app/assets/css/**/*.css',
-            'app/assets/imgs/**/*.png',
-            'app/assets/js/**/*.js'
+            'dist/css/**/*.css',
+            'dist/**/*.js'
     ];
 
     browserSync.init(files, {
             server: {
-                 baseDir: '.'
+                 baseDir: 'dist'
             }
     });
 });
+
+gulp.task('watch', ['browser-sync', 'build'], function () {
+    gulp.watch(SRC.JADE, ['build-jade']);
+    gulp.watch(SRC.SASS, ['build-sass']);
+    gulp.watch(SRC.JS, ['build-js']);
+});
+
+
+
+gulp.task('clean', function () {
+    return gulp.src('dist/**/*', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('pre-build', function () {
+    return gulp.src('src/CNAME')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', function (callback) {
+    runSequence('clean', 'pre-build', ['build-sass', 'build-jade', 'build-js'], callback);
+});
+
+gulp.task('default', ['build']);
 
 
 var deploy = require("gulp-gh-pages-ci-compatible");
@@ -49,7 +80,7 @@ var options = {
     message: 'Update ' + new Date().toISOString() + ' [skip ci]',
     branch: "master"
 };
-gulp.task('deploy', function () {
+gulp.task('deploy', ['build'], function () {
     gulp.src("dist/**/*")
         .pipe(deploy(options));
 });
