e56654871357d0df03365bbb609d5775f854420c
Mark Mironyuk <contact@markmiro.com>
2018-05-26 22:31:37 -0700

    Add a userscript for website that I use commonly

---
+++ reconsider/reconsider.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @match https://www.youtube.com/*
+// @match https://mail.google.com/*
+// @match https://inbox.google.com/*
+// @match https://medium.com/*
+// @match https://news.ycombinator.com/*
+// @match https://www.facebook.com/*
+// ==/UserScript==
+
+// https://www.chromium.org/developers/design-documents/user-scripts
+
+alert('reconsider');
